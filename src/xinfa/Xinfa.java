5664d2af3771e1d7c6063b804027ec17889daa78
zhaoxin11112222 <zhaoxin11112222@163.com>
2017-08-17 21:10:18 +0800

    修改

--- src/xinfa/Xinfa.java
+++ src/xinfa/Xinfa.java
@@ -4,5 +4,7 @@ public class Xinfa {
 	public static void main(String[] args) {
 		System.out.println("xiaowuxianggong");
 		System.out.println("xiaowuxianggong");
+		System.out.println("xiaowuxianggong");
+		System.out.println("xiaowuxianggong");
 	}
 }
